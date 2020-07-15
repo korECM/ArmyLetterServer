22f7d36470cba395719e7d73c078c685924e5eb7
JongwonYoun <jeffyoun@naver.com>
2020-07-16 03:04:11 +0900

    console.error console.log로 변경

--- src/module/Service/ArmyService/addSoldier.ts
+++ src/module/Service/ArmyService/addSoldier.ts
@@ -1,27 +1,27 @@
-import { ArmySoldier, Cookie } from "../../Models";
-import requestPromise from "request-promise";
-import { addCookie, checkDate } from "../../Utils";
+import { ArmySoldier, Cookie } from '../../Models';
+import requestPromise from 'request-promise';
+import { addCookie, checkDate } from '../../Utils';
 
 async function addSoldier(soldier: ArmySoldier, cookie: Cookie) {
   if (!soldier) {
-    throw new Error("유효하지 않은 군인 정보입니다");
+    throw new Error('유효하지 않은 군인 정보입니다');
   }
   if (!cookie || !cookie.iuid || !cookie.token) {
-    throw new Error("로그인 정보가 없습니다");
+    throw new Error('로그인 정보가 없습니다');
   }
   if (!checkDate(soldier.birthDate)) {
-    throw new Error("유효하지 않은 생일");
+    throw new Error('유효하지 않은 생일');
   }
   if (!checkDate(soldier.enterDate)) {
-    throw new Error("유효하지 않은 입영일");
+    throw new Error('유효하지 않은 입영일');
   }
   let cookieJar = addCookie(cookie);
   const options = {
-    url: "https://www.thecamp.or.kr/missSoldier/insertDirectMissSoldierA.do",
-    method: "POST",
+    url: 'https://www.thecamp.or.kr/missSoldier/insertDirectMissSoldierA.do',
+    method: 'POST',
     json: true,
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
     jar: cookieJar,
     form: {
@@ -44,21 +44,17 @@ async function addSoldier(soldier: ArmySoldier, cookie: Cookie) {
       throw new Error(err);
     }
     // console.log(res.statusCode, `-`, res.statusMessage, " ", body);
-    if (
-      res.statusCode === 200 &&
-      body.resultCd !== "0000" &&
-      body.resultCd !== "E001"
-    ) {
-      error = new Error("모르는 에러");
+    if (res.statusCode === 200 && body.resultCd !== '0000' && body.resultCd !== 'E001') {
+      error = new Error('모르는 에러');
       // throw new Error("모르는 에러");
     }
-    if (res.statusCode === 200 && body.resultCd === "E001") {
+    if (res.statusCode === 200 && body.resultCd === 'E001') {
       // throw new Error(body.resultMsg);
-      console.error(body.resultMsg);
+      console.log(soldier.name, ' : ', body.resultMsg);
     }
   });
   if (!response) {
-    console.error("응답 없음");
+    console.error('응답 없음');
   }
   if (error) {
     throw error;
