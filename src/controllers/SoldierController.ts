bb24d6d47f3cc3ef2c8a2424c84780173e5a62d8
JongwonYoun <jeffyoun@naver.com>
2020-07-17 04:51:59 +0900

    Soldier GET 기본 라우터

---
+++ src/controllers/SoldierController.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+import { param, query, validationResult } from 'express-validator';
+import { Types } from 'mongoose';
+import { findArmySoldier, findAirForceSoldier } from '../services/FindSoldier';
+import { ArmySoldierInterface, ArmySoldier } from '../module/MIL/Models/Army';
+import { AirForceSoldierInterface, AirForceSoldier } from '../module/MIL/Models';
+import ArmySoldierModel from '../models/ArmySoldier';
+import AirForceSoldierModel from '../models/AirForceSoldier';
+
+let { ObjectId } = Types;
+
+export let getSoldierValidator = [param('id').notEmpty(), query('type').notEmpty().isIn(['airForce', 'army'])];
+
+export async function getSoldierProxy(req: Request, res: Response, next: NextFunction) {
+  if (!ObjectId.isValid(req.params.id!)) return res.status(406).send();
+
+  const soldier = await getSoldier(req.params.id!, req.query.type as string);
+
+  if (!soldier) return res.status(406).send();
+
+  return res.status(200).json(soldier);
+}
+
+export async function getSoldier(soldierID: string, soldierType: string) {
+  switch (soldierType) {
+    case 'airForce':
+      return await getAirForceSoldier(soldierID);
+    case 'army':
+      return await getArmySoldier(soldierID);
+      break;
+    default:
+      return null;
+  }
+
+  return null;
+}
+
+async function getArmySoldier(soldierID: string) {
+  let soldier = await ArmySoldierModel.findOne({ _id: soldierID }).exec();
+  return soldier;
+}
+
+async function getAirForceSoldier(soldierID: string) {
+  let soldier = await AirForceSoldierModel.findOne({ _id: soldierID }).exec();
+  return soldier;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,15 @@
 import { Request, Response, Application } from 'express';
 import { createArmySoldierValidator, createArmySoldierProxy, createAirSoldierProxy, createAirSoldierValidator } from '../controllers/MainController';
 import { rejectInvalidForm } from '../util/route';
+import { getSoldierValidator, getSoldierProxy } from '../controllers/SoldierController';
 
 export class Routes {
   public routes(app: Application): void {
     app.route('/main/army').post(createArmySoldierValidator, rejectInvalidForm(), createArmySoldierProxy);
 
     app.route('/main/air').post(createAirSoldierValidator, rejectInvalidForm(), createAirSoldierProxy);
+
+    app.route('/soldier/:id').get(getSoldierValidator, rejectInvalidForm(), getSoldierProxy);
 
     app.route('/').get((req: Request, res: Response) => {
       res.status(200).send({
