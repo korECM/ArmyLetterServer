eb94b75e0ff831be315adcfe3c02293239ef224e
JongwonYoun <jeffyoun@naver.com>
2020-07-16 16:19:43 +0900

    DB Connect 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from './app';
+import DB from './DB';
 import { createServer } from 'http';
 
 (async () => {
+  await DB.connect();
+
   if (process.env.NODE_ENV !== 'test') {
     createServer(app).listen(app.get('port'), () => {
       console.log(`서버 Port : ${app.get('port')}에서 시작`);
