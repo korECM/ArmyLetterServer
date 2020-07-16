d5798893364b0b2693a57a119d8df763532974d6
JongwonYoun <jeffyoun@naver.com>
2020-07-16 15:16:27 +0900

    테스트 환경에서만 서버 실행

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import app from './app';
 import { createServer } from 'http';
 
 (async () => {
-  createServer(app).listen(app.get('port'), () => {
-    console.log(`서버 Port : ${app.get('port')}에서 시작`);
-  });
+  if (process.env.NODE_ENV !== 'test') {
+    createServer(app).listen(app.get('port'), () => {
+      console.log(`서버 Port : ${app.get('port')}에서 시작`);
+    });
+  }
 })();
 
 export { app };
