090cabd982c2429547bc9095317727cb4187a8d0
JongwonYoun <jeffyoun@naver.com>
2020-07-18 02:38:20 +0900

    오타로 인한 테스트 실패 수정

--- src/services/GetSoldierFromDB.ts
+++ src/services/GetSoldierFromDB.ts
@@ -1,12 +1,23 @@
 import ArmySoldierModel from '../models/ArmySoldier';
 import AirForceSoldierModel from '../models/AirForceSoldier';
+require('../models/Sports');
+
+// type soldierPopulate = 'sports';
 
 export async function getArmySoldier(soldierID: string) {
-  let soldier = await ArmySoldierModel.findOne({ _id: soldierID }).exec();
-  return soldier;
+  return await ArmySoldierModel.findOne({ _id: soldierID }).exec();
 }
 
 export async function getAirForceSoldier(soldierID: string) {
-  let soldier = await AirForceSoldierModel.findOne({ _id: soldierID }).exec();
+  return await AirForceSoldierModel.findOne({ _id: soldierID }).exec();
+}
+
+export async function getArmySoldierWithSports(soldierID: string) {
+  let soldier = await ArmySoldierModel.findOne({ _id: soldierID }).populate('sports').exec();
+  return soldier;
+}
+
+export async function getAirForceSoldierWithSports(soldierID: string) {
+  let soldier = await AirForceSoldierModel.findOne({ _id: soldierID }).populate('sports').exec();
   return soldier;
 }
