d2f887bf2682f6de81f6cab65e00b6ef7516cbce
JongwonYoun <jeffyoun@naver.com>
2020-07-21 02:27:15 +0900

    공군 편지 보내기 추가

--- src/services/sendLetter.ts
+++ src/services/sendLetter.ts
@@ -2,7 +2,14 @@ import Letter, { LetterSchemaInterface } from '../models/Letter';
 import ArmySoldier from '../models/ArmySoldier';
 import AirForceSoldier from '../models/AirForceSoldier';
 import { findArmySoldier } from './FindSoldier';
-import { ArmyLetter, ArmySoldierInterface, ArmySoldier as ArmySoldierMIL } from '../module/MIL/Models';
+import {
+  ArmyLetter,
+  ArmySoldierInterface,
+  ArmySoldier as ArmySoldierMIL,
+  AirForceSoldierInterface,
+  AirForceLetter,
+  AirForceSoldier as AirForceSoldierMIL,
+} from '../module/MIL/Models';
 import { MilitaryLetter } from '../module/MIL/Service/MilitaryLetter';
 
 export async function sendLetterInDBToSoldiers() {
@@ -42,7 +49,28 @@ export async function sendLetterInDBToSoldiers() {
     // TODO: 공군 전송 기능
     let airForceSoldier = await AirForceSoldier.find({ corona: true }).populate('letters').exec();
 
-    airForceSoldier.forEach(async (soldier) => {});
+    airForceSoldier.forEach(async (soldier) => {
+      console.log(`${soldier.name} 편지 전송 시작`);
+      (soldier.letters as LetterSchemaInterface[])
+        .filter((letter) => letter.sended === false)
+        .forEach(async (letter) => {
+          console.log(`${letter.title} 전송 시작`);
+          await sendAirForceSoldierLetter(soldier, {
+            addr1: '',
+            addr2: '',
+            body: letter.body,
+            relationship: '인편 대행',
+            senderName: '인편 대행 서비스',
+            password: 'ArmyLetterService',
+            title: letter.title,
+            zipCode: '',
+          });
+          letter.sended = true;
+          await letter.save();
+          console.log(`${letter.title} 전송 끝`);
+        });
+      console.log(`${soldier.name} 편지 전송 끝`);
+    });
 
     console.log('편지 전송 끝');
   } catch (error) {
@@ -72,18 +100,35 @@ export async function sendArmySoldierLetter(soldier: ArmySoldierInterface, lette
   }
 }
 
-export async function sendLetterToSoldier(soldierID: string, soldierType: string, letter: ArmyLetter, nickName: string = '인편 대행 서비스') {
+export async function sendAirForceSoldierLetter(soldier: AirForceSoldierInterface, letter: AirForceLetter) {
+  const ml = new MilitaryLetter();
+  try {
+    let targetSoldier: AirForceSoldierMIL = new AirForceSoldierMIL({
+      ...soldier,
+      birthDate: soldier.birthDate.replace(/-/g, ''),
+    });
+
+    await ml.setSoldier(targetSoldier);
+
+    await ml.sendLetter(letter);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function sendLetterToSoldier(soldierID: string, soldierType: string, letter: ArmyLetter | AirForceLetter, nickName: string = '인편 대행 서비스') {
   switch (soldierType) {
     case 'army':
-      let soldier = await ArmySoldier.findOne({ _id: soldierID }).exec();
-      if (!soldier) return;
+      let armySoldier = await ArmySoldier.findOne({ _id: soldierID }).exec();
+      if (!armySoldier) return;
       await sendArmySoldierLetter(
         {
           armyType: '육군',
-          armyUnit: soldier.armyUnit,
-          birthDate: soldier.birthDate,
-          enterDate: soldier.enterDate,
-          name: soldier.name,
+          armyUnit: armySoldier.armyUnit,
+          birthDate: armySoldier.birthDate,
+          enterDate: armySoldier.enterDate,
+          name: armySoldier.name,
           relationship: '친구/지인',
           soldierType: '예비군인/훈련병',
         },
@@ -92,7 +137,9 @@ export async function sendLetterToSoldier(soldierID: string, soldierType: string
       );
       break;
     case 'airForce':
-      // await send
+      let airForceSoldier = await AirForceSoldier.findOne({ _id: soldierID }).exec();
+      if (!airForceSoldier) return;
+      await sendAirForceSoldierLetter(airForceSoldier, letter as AirForceLetter);
       break;
     default:
       break;
