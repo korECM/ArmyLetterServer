ef33747b682882af0137a5303702b23eebaff1de
JongwonYoun <jeffyoun@naver.com>
2020-07-21 02:16:37 +0900

    육군 편지 전송 함수 옮김

--- src/services/sendLetter.ts
+++ src/services/sendLetter.ts
@@ -1,8 +1,9 @@
 import Letter, { LetterSchemaInterface } from '../models/Letter';
 import ArmySoldier from '../models/ArmySoldier';
 import AirForceSoldier from '../models/AirForceSoldier';
-import { findArmySoldier, sendArmySoldierLetter } from './FindSoldier';
-import { ArmyLetter } from '../module/MIL/Models';
+import { findArmySoldier } from './FindSoldier';
+import { ArmyLetter, ArmySoldierInterface, ArmySoldier as ArmySoldierMIL } from '../module/MIL/Models';
+import { MilitaryLetter } from '../module/MIL/Service/MilitaryLetter';
 
 export async function sendLetterInDBToSoldiers() {
   console.log('편지 전송 시작');
@@ -29,6 +30,7 @@ export async function sendLetterInDBToSoldiers() {
               relationship: '친구/지인',
             },
             new ArmyLetter(letter.title, letter.body),
+            '인편 대행 서비스',
           );
           letter.sended = true;
           await letter.save();
@@ -45,5 +47,54 @@ export async function sendLetterInDBToSoldiers() {
     console.log('편지 전송 끝');
   } catch (error) {
     console.error(error);
+  }
+}
+
+export async function sendArmySoldierLetter(soldier: ArmySoldierInterface, letter: ArmyLetter, nickName: string) {
+  const ml = new MilitaryLetter();
+  try {
+    await ml.config(process.env.ID!, process.env.PW!);
+
+    let targetSoldier: ArmySoldierMIL = new ArmySoldierMIL({
+      ...soldier,
+      relationship: '친구/지인',
+      soldierType: '예비군인/훈련병',
+    });
+
+    await ml.updateNickname(nickName);
+
+    await ml.setSoldier(targetSoldier);
+
+    await ml.sendLetter(letter);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function sendLetterToSoldier(soldierID: string, soldierType: string, letter: ArmyLetter, nickName: string = '인편 대행 서비스') {
+  switch (soldierType) {
+    case 'army':
+      let soldier = await ArmySoldier.findOne({ _id: soldierID }).exec();
+      if (!soldier) return;
+      await sendArmySoldierLetter(
+        {
+          armyType: '육군',
+          armyUnit: soldier.armyUnit,
+          birthDate: soldier.birthDate,
+          enterDate: soldier.enterDate,
+          name: soldier.name,
+          relationship: '친구/지인',
+          soldierType: '예비군인/훈련병',
+        },
+        letter,
+        nickName,
+      );
+      break;
+    case 'airForce':
+      // await send
+      break;
+    default:
+      break;
   }
 }
