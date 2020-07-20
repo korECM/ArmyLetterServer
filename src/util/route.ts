5ee757dda1f6b3a2ec113a70c5ec723978816a9c
JongwonYoun <jeffyoun@naver.com>
2020-07-21 02:44:35 +0900

    rejectInvalidForm 에러 출력하도록

--- src/util/route.ts
+++ src/util/route.ts
@@ -4,6 +4,7 @@ export function rejectInvalidForm(status: number = 400) {
   return function (req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.error(errors);
       return res.status(400).send();
     }
     next();
