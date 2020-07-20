257a1bebb994bce02a31d196e53d8e391ed1a05e
JongwonYoun <jeffyoun@naver.com>
2020-07-21 02:37:11 +0900

    sendLetter router 완성

--- src/controllers/SendLetter.ts
+++ src/controllers/SendLetter.ts
@@ -1,20 +1,47 @@
 import { Request, Response, NextFunction } from 'express';
-import { param, query } from 'express-validator';
+import { param, query, body } from 'express-validator';
 import { Types } from 'mongoose';
 import { getAirForceSoldier, getArmySoldier } from '../services/GetSoldierFromDB';
+import { sendAirForceSoldierLetter, sendArmySoldierLetter } from '../services/sendLetter';
+import { AirForceLetter, ArmyLetter, ArmySoldierInterface } from '../module/MIL/Models';
+import { ArmySoldierSchemaInterface } from '../models/ArmySoldier';
 
 let { ObjectId } = Types;
 
-export let getSoldierValidator = [param('id').notEmpty(), query('type').notEmpty().isIn(['airForce', 'army'])];
+export let sendLetterValidator = [
+  param('id').notEmpty(),
+  query('type').notEmpty().isIn(['airForce', 'army']),
+  body('title').notEmpty(),
+  body('body').notEmpty(),
+];
 
 export async function sendLetterProxy(req: Request, res: Response, next: NextFunction) {
   if (!ObjectId.isValid(req.params.id!)) return res.status(406).send();
 
   const soldier = await getSoldier(req.params.id!, req.query.type as string);
-
-  
-
   if (!soldier) return res.status(406).send();
+
+  let { title, body, sender, password, relationship } = req.body;
+
+  if (req.query.type === 'airForce') {
+    if (!sender || !password || !relationship) return res.status(400).send();
+    await sendAirForceSoldierLetter(soldier, new AirForceLetter(title, body, sender, relationship, '', '', '', password));
+  } else {
+    //TODO: 보내는 사람 이름 설정
+    await sendArmySoldierLetter(
+      {
+        armyType: '육군',
+        armyUnit: (soldier as ArmySoldierSchemaInterface).armyUnit,
+        birthDate: soldier.birthDate,
+        enterDate: soldier.enterDate,
+        name: soldier.name,
+        relationship: '친구/지인',
+        soldierType: '예비군인/훈련병',
+      },
+      new ArmyLetter(title, body),
+      '',
+    );
+  }
 
   return res.status(200).json(soldier);
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createArmySoldierValidator, createArmySoldierProxy, createAirSoldierPro
 import { rejectInvalidForm } from '../util/route';
 import { getSoldierValidator, getSoldierProxy } from '../controllers/SoldierController';
 import { postSubValidator, postSubscriptionProxy } from '../controllers/SubscriptController';
+import { sendLetterValidator, sendLetterProxy } from '../controllers/SendLetter';
 
 export class Routes {
   public routes(app: Application): void {
@@ -11,6 +12,8 @@ export class Routes {
     app.route('/main/air').post(createAirSoldierValidator, rejectInvalidForm(), createAirSoldierProxy);
 
     app.route('/soldier/:id').get(getSoldierValidator, rejectInvalidForm(), getSoldierProxy);
+
+    app.route('/soldier/send/:id').post(sendLetterValidator, rejectInvalidForm(), sendLetterProxy);
 
     app.route('/subscript/:id').post(postSubValidator, rejectInvalidForm(), postSubscriptionProxy);
 
