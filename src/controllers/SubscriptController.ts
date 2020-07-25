3c1c8f72c6ce672884093d91b1634032515d1249
JongwonYoun <jeffyoun@naver.com>
2020-07-25 22:14:44 +0900

    subscription controller 제외 주석 처리

--- src/controllers/SubscriptController.ts
+++ src/controllers/SubscriptController.ts
@@ -1,12 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { param, query, body } from 'express-validator';
 import { Types } from 'mongoose';
-import { ArmySoldierInterface, ArmySoldierMIL } from '../module/MIL/Models/Army';
-import ArmySoldierModel, { ArmySoldierSchemaInterface } from '../models/ArmySoldier';
-import AirForceSoldierModel, { AirForceSchemaInterface } from '../models/AirForceSoldier';
-import { getAirForceSoldier, getArmySoldier, getAirForceSoldierWithSports, getArmySoldierWithSports } from '../services/GetSoldierFromDB';
-import Sports, { SportsSchemaInterface } from '../models/Sports';
-import { saveSoldier } from '../services/SaveSoldier';
 import { SoldierService } from '../services/Soldier/SoldierService';
 
 let { ObjectId } = Types;
@@ -27,56 +21,56 @@ export async function postSubscriptionProxy(req: Request, res: Response, next: N
   }
 }
 
-export async function postSubscription(soldierID: string, soldierType: string, subscription: SubscriptionRequestInterface) {
-  let soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface | null = await requestFindSoldier(soldierID, soldierType);
+// export async function postSubscription(soldierID: string, soldierType: string, subscription: SubscriptionRequestInterface) {
+//   let soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface | null = await requestFindSoldier(soldierID, soldierType);
 
-  if (soldier === null) return null;
+//   if (soldier === null) return null;
 
-  await updateSportsInformation(soldier, subscription);
+//   await updateSportsInformation(soldier, subscription);
 
-  await updateNewsInformation(soldier, subscription);
+//   await updateNewsInformation(soldier, subscription);
 
-  await saveSoldier(soldier, soldierType);
-}
+//   await saveSoldier(soldier, soldierType);
+// }
 
-export async function updateNewsInformation(soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface, subscription: SubscriptionRequestInterface) {
-  const { news } = subscription;
+// export async function updateNewsInformation(soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface, subscription: SubscriptionRequestInterface) {
+//   const { news } = subscription;
 
-  soldier.news = news;
-}
+//   soldier.news = news;
+// }
 
-export async function updateSportsInformation(soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface, subscription: SubscriptionRequestInterface) {
-  let sports: SportsSchemaInterface | null;
+// export async function updateSportsInformation(soldier: AirForceSchemaInterface | ArmySoldierSchemaInterface, subscription: SubscriptionRequestInterface) {
+//   let sports: SportsSchemaInterface | null;
 
-  // undefined or null
-  if (!soldier.sports) {
-    console.log('군인 Sports 정보 없음');
-    sports = new Sports();
-    soldier.sports = sports._id;
-  } else {
-    sports = soldier.sports;
-  }
+//   // undefined or null
+//   if (!soldier.sports) {
+//     console.log('군인 Sports 정보 없음');
+//     sports = new Sports();
+//     soldier.sports = sports._id;
+//   } else {
+//     sports = soldier.sports;
+//   }
 
-  const { koreaBaseball, koreaBasketball, koreaSoccer, worldBaseball, worldBasketball, worldSoccer, esports } = subscription.sports;
+//   const { koreaBaseball, koreaBasketball, koreaSoccer, worldBaseball, worldBasketball, worldSoccer, esports } = subscription.sports;
 
-  sports.koreaBaseball = koreaBaseball;
-  sports.koreaBasketball = koreaBasketball;
-  sports.koreaSoccer = koreaSoccer;
-  sports.worldBaseball = worldBaseball;
-  sports.worldBasketball = worldBasketball;
-  sports.worldSoccer = worldSoccer;
-  sports.esports = esports;
+//   sports.koreaBaseball = koreaBaseball;
+//   sports.koreaBasketball = koreaBasketball;
+//   sports.koreaSoccer = koreaSoccer;
+//   sports.worldBaseball = worldBaseball;
+//   sports.worldBasketball = worldBasketball;
+//   sports.worldSoccer = worldSoccer;
+//   sports.esports = esports;
 
-  await sports.save();
-}
+//   await sports.save();
+// }
 
-async function requestFindSoldier(soldierID: string, soldierType: string) {
-  switch (soldierType) {
-    case 'airForce':
-      return await getAirForceSoldierWithSports(soldierID);
-    case 'army':
-      return await getArmySoldierWithSports(soldierID);
-    default:
-      return null;
-  }
-}
+// async function requestFindSoldier(soldierID: string, soldierType: string) {
+//   switch (soldierType) {
+//     case 'airForce':
+//       return await getAirForceSoldierWithSports(soldierID);
+//     case 'army':
+//       return await getArmySoldierWithSports(soldierID);
+//     default:
+//       return null;
+//   }
+// }
