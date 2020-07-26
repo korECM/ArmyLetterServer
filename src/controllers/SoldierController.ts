f589bd2d6190c2c6f981299de1796ff62bef8746
JongwonYoun <jeffyoun@naver.com>
2020-07-27 02:48:08 +0900

    getSoldierProxy 주석 제거하고 promise await 안한 버그 수정

--- src/controllers/SoldierController.ts
+++ src/controllers/SoldierController.ts
@@ -10,26 +10,12 @@ export let getSoldierValidator = [param('id').notEmpty(), query('type').notEmpty
 export async function getSoldierProxy(req: Request, res: Response, next: NextFunction) {
   if (!ObjectId.isValid(req.params.id!)) return res.status(406).send();
 
-  // const soldier = await getSoldier(req.params.id!, req.query.type as string);
   let controller = SoldierService.getSoldierController(req.query.type as string);
   if (!controller) return res.status(406).send();
 
-  const soldier = controller.getDBSoldierById(req.params.id, 'letters');
+  const soldier = await controller.getDBSoldierById(req.params.id, 'letters');
 
   if (!soldier) return res.status(406).send();
 
   return res.status(200).json(soldier);
 }
-
-// export async function getSoldier(soldierID: string, soldierType: string) {
-//   switch (soldierType) {
-//     case 'airForce':
-//       return await getAirForceSoldierWithLetters(soldierID);
-//     case 'army':
-//       return await getArmySoldierWithLetters(soldierID);
-//     default:
-//       return null;
-//   }
-
-//   return null;
-// }
