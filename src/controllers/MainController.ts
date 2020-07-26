08ae37e9e8af2a42f3bbbcc2a09b8af359c25d23
JongwonYoun <jeffyoun@naver.com>
2020-07-27 02:44:51 +0900

    주석 제거

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -12,7 +12,6 @@ export let createArmySoldierValidator = [
 ];
 
 export async function createArmySoldierProxy(req: Request, res: Response, next: NextFunction) {
-  // const soldier = await createArmySoldier(req.body);
   let soldierController = new ArmySoldierService();
   let existSoldier = await soldierController.findSoldier(req.body.name, req.body.birthDate);
   if (existSoldier) {
@@ -26,34 +25,9 @@ export async function createArmySoldierProxy(req: Request, res: Response, next: 
   return res.status(201).json(soldier);
 }
 
-// export async function createArmySoldier(soldier: ArmySoldierInterface) {
-//   const soldierFromArmy = await findArmySoldier(soldier);
-//   if (soldierFromArmy) {
-//     const { name, birthDate, enterDate, endDate, trainUnitCdName: armyUnit, trainUnitEdNm } = soldierFromArmy as ArmySoldierMIL;
-
-//     let exSoldier = await ArmySoldierModel.findOne({ name, birthDate, enterDate, armyUnit, trainUnitEdNm, endDate });
-//     if (exSoldier) return exSoldier;
-
-//     let dbSoldier = new ArmySoldierModel({
-//       name,
-//       birthDate,
-//       enterDate,
-//       armyUnit,
-//       trainUnitEdNm,
-//       endDate,
-//       letters: [],
-//     });
-//     await dbSoldier.save();
-//     return dbSoldier;
-//   }
-
-//   return null;
-// }
-
 export let createAirSoldierValidator = [body('name').notEmpty(), body('birthDate').notEmpty().isLength({ max: 10, min: 10 })];
 
 export async function createAirSoldierProxy(req: Request, res: Response, next: NextFunction) {
-  // const soldier = await createAirSoldier(req.body);
   let soldierController = new AirForceSoldierService();
   let existSoldier = await soldierController.findSoldier(req.body.name, req.body.birthDate);
   if (existSoldier) {
@@ -65,28 +39,3 @@ export async function createAirSoldierProxy(req: Request, res: Response, next: N
 
   return res.status(201).json(soldier);
 }
-
-// export async function createAirSoldier(soldier: AirForceSoldierInterface) {
-//   const soldierFromAirForce = await findAirForceSoldier(soldier);
-
-//   if (soldierFromAirForce) {
-//     const { name, birthDate: birthDateRaw, enterDate, endDate, imageURL: image, soldierInfo: trainUnitEdNm } = soldierFromAirForce as AirForceSoldier;
-//     let birthDate = [birthDateRaw.substring(0, 4), birthDateRaw.substring(4, 6), birthDateRaw.substring(6)].join('-');
-//     let exSoldier = await AirForceSoldierModel.findOne({ name, birthDate, enterDate, trainUnitEdNm, endDate, image });
-//     if (exSoldier) return exSoldier;
-
-//     let dbSoldier = new AirForceSoldierModel({
-//       name,
-//       birthDate,
-//       enterDate,
-//       trainUnitEdNm,
-//       endDate,
-//       image,
-//       letters: [],
-//     });
-//     await dbSoldier.save();
-//     return dbSoldier;
-//   }
-
-//   return soldierFromAirForce;
-// }
