8e0985a9c44e38049cb89ba373d5366a4763e85e
JongwonYoun <jeffyoun@naver.com>
2020-07-26 23:32:13 +0900

    AirForceSoldierService 기본 형태 완성

---
+++ src/services/Soldier/AirForceSolderService.ts
@@ -0,0 +1,38 @@
+import { SoldierService, SubscriptionRequestInterface } from './SoldierService';
+import { AirForceSoldierDBInterface, AirForceSoldierDB, AirForceSchemaColumnsInterface, AirForceSchemaInterface } from '../../models/AirForceSoldier';
+import { IMilitaryLetter } from '../../module/MIL/Service/IMilitaryLetter';
+import { MilitaryLetter, AirForceSoldier } from '../../module/MIL/Service/MilitaryLetter';
+import { AirForceSoldierInterface, AirForceLetter } from '../../module/MIL/Models';
+
+export class AirForceSoldierService extends SoldierService {
+  constructor(
+    private AirForceSoldierDBModel: AirForceSoldierDBInterface = new AirForceSoldierDB(),
+    private militaryLetter: IMilitaryLetter = new MilitaryLetter(),
+  ) {
+    super();
+  }
+
+  async getDBSoldierById(id: string): Promise<AirForceSchemaColumnsInterface | null> {
+    return null;
+  }
+
+  async getMILSoldierByDBSoldier(soldier: AirForceSchemaColumnsInterface | string): Promise<AirForceSoldier | null> {
+    return null;
+  }
+
+  async createDBSoldier(soldier: AirForceSoldierInterface): Promise<AirForceSchemaInterface | null> {
+    return null;
+  }
+
+  async checkMILSoldierExistInSiteByDBSoldier(soldier: AirForceSoldierInterface): Promise<boolean> {
+    return true;
+  }
+
+  async sendLetter(soldier: AirForceSchemaColumnsInterface | string, letter: AirForceLetter): Promise<boolean> {
+    return true;
+  }
+
+  async updateSubscription(soldier: string, subscription: SubscriptionRequestInterface): Promise<boolean> {
+    return true;
+  }
+}
